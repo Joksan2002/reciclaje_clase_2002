df2fee12c92ed7b7c5b326fe90b81905144e8d87
Joksan2002 <113870678+Joksan2002@users.noreply.github.com>
2026-06-23 10:36:10 -0600

    Add model configuration for MobileNetV2

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ from PIL import Image
 st.set_page_config(page_title="Reciclaje IA-ISC", layout="centered")
 st.title("Modelo predictivo Reciclaje clase de IA-ISC-Campus Comayagua-2026-Joksan-Zavala")
 st.write("Suba una imagen para clasificar con el modelo MobileNetV2 pre entrenado")
+
+IMG_SIZE = (224, 224)
+MODEL_DIR=Path("modelo_reciclaje_mobilenet")
+CLASS_PATH=MODEL_DIR/"class_names.json"
+MODEL_PATH=[MODEL_DIR/"waste_mobilenet.h5", MODEL_DIR/"waste_mobilenet.keras"]
