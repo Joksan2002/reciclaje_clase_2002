3f8726216d1c72c0d29b93dec87c532d4db3d99b
Joksan2002 <113870678+Joksan2002@users.noreply.github.com>
2026-06-23 10:52:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ LABELS_ES={
 
 @st.cache_resource
 def cargar_modelo():
-    for path in MODEL_PATHS:
+    for path in MODEL_PATH:
         if path.exists():
             return tf.keras.models.load_model(path, compile=False)
     st.error("No se encontró el modelo. Coloque la carpeta modelo_reciclaje_mobilenet junto a app.py.")
